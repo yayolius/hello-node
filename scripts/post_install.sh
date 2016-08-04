c1b21cba16a66aa4b5cded04abd698ca182e7bc2
Claudio Castro <yayolius@gmail.com>
2016-08-04 10:17:11 -0400

    because base path is still with root, just create the folders that we need

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 source /home/ubuntu/.bash_profile
 cd /home/ubuntu/hello-node/
+mkdir /home/ubuntu/hello-node/node_modules
+chown ubuntu:ubuntu /home/ubuntu/hello-node/node_modules
+chmod 777 /home/ubuntu/hello-node/node_modules
 npm install
