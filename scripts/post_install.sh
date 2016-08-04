8d36ce117f0968b2cd134cfa53d5f061ef8e45d6
Claudio Castro <yayolius@gmail.com>
2016-08-04 10:44:24 -0400

    when files are copied the onwership of the folder is changed back to root

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 source /home/ubuntu/.bash_profile
 
-# I want to make sure that the directory is clean and has nothing left over from
-# previous deployments. The servers auto scale so the directory may or may not
-# exist.
-if [ -d /home/ubuntu/hello-node ]; then
-    rm -rf /home/ubuntu/hello-node
-fi
-
-mkdir -vp /home/ubuntu/hello-node
-chown ubuntu:ubuntu /home/ubuntu/hello-node
 cd /home/ubuntu/hello-node
 npm install
