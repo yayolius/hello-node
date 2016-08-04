9477699b4644c1d603b00ccd8e4bfadf4ef34040
Claudio Castro <yayolius@gmail.com>
2016-08-04 10:35:24 -0400

    because base path is still with root, just create the folders that we need

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,3 +3,13 @@ curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 sudo npm install -g pm2 gulp
+
+# I want to make sure that the directory is clean and has nothing left over from
+# previous deployments. The servers auto scale so the directory may or may not
+# exist.
+if [ -d /home/ubuntu/hello-node ]; then
+    rm -rf /home/ubuntu/hello-node
+fi
+
+mkdir -vp /home/ubuntu/hello-node
+chown ubuntu:ubuntu /home/ubuntu/hello-node
